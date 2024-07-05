75419b5ba3df7ccecbcaea034ac9da23167ec7d1
Sebastian Dowell <sebastiandowell@Sebastians-MacBook-Air-0.local>
2024-07-05 01:11:53 -0700

    temporarily comment robotcontainer in Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,11 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private RobotContainer m_robotContainer;
+  // private RobotContainer m_robotContainer;
 
   @Override
   public void robotInit() {
-    m_robotContainer = new RobotContainer();
+    // m_robotContainer = new RobotContainer();
   }
 
   @Override
